0f623589bfb9247897198be403a0fe35ee3dfd94
Manuel Perdomo <manuelperdomo1710@icloud.com>
2020-07-27 17:00:51 -0500

    Definido esquema de colores

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,58 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  :root {
+    --dark-blue: #121232;
+    --medium-blue: #1c1e45;
+    --light-blue: #32436f;
+    --other-blue: #1d8cf8;
+    --white-color: #e9f7ff;
+    --light-gray: #9a9694;
+    --intense-gray: #344675;
+    --intense-coral: #ff8d72;
+    --light-coral: #46304a;
+    --intense-green: #00f2c3;
+    --light-green: #12455b;
+    --intense-purple: #e14eca;
+    --light-purple: #40235c;
+    --intense-pink: #fd5d93;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  :global(body) {
+    background-color: var(--dark-blue);
+    color: var(--light-gray);
+  }
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  a {
+    color: var(--intense-purple);
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: var(--intense-coral);
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
