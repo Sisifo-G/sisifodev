9eabd8186e291de3e478f5481e544f0fdc496a14
juanestban <53704567+Juanestban@users.noreply.github.com>
2020-07-27 15:10:13 -0500

    Implement components created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Componenets
+import Header from './components/header/Header';
+import Navegation from './components/navigation/Navigation';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navegation background="bg-primary" titleNav="Esto es un Menu de Navegacion" />
     </div>
   );
 }
