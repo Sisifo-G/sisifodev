285016e3cae82d3ceb1a9fa8164dbcf07edf693c
juanestban <53704567+Juanestban@users.noreply.github.com>
2020-07-28 17:37:56 -0500

    Edditing Component navigation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import $ from 'jquery';
+import './Navigation.css';
 
-const Navigation = (props) => {
-    return(
-        <div className={ props.background }>
-            <div className="container p-2">
-                <h3 className="text-white">{ props.titleNav }</h3>
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'Juan'
+        }
+    }
+
+    handleButton = () => {
+        $('.dato').on('click', () => {
+            console.log('Clicked');
+        })
+    }
+
+    render() {
+        return (
+            <div className="">
+                <div className="container bg-primary p-2">
+                    <button id="dato" className="btn dato">{this.props.titleNav}</button>
+                    <button className="btn">{this.props.titleNav}</button>
+                    <button className="btn">{this.props.titleNav}</button>
+                    <button className="btn">{this.props.titleNav}</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+// const Navigation = (props) => {
+//     return(
+//         <div className="">
+//             <div className="container bg-primary p-2">
+//                 <button id="dato" className="btn">{ props.titleNav }</button>
+//                 <button className="btn">{ props.titleNav }</button>
+//                 <button className="btn">{ props.titleNav }</button>
+//                 <button className="btn">{ props.titleNav }</button>
+//             </div>
+//         </div>
+//     );
+// }
 
 export default Navigation;
